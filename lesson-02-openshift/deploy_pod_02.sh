af1f3d694af2688fb11d25f88be9d4bf53d380d1
Bharath <bharathdcs@gmail.com>
2025-02-25 13:56:18 +1100

    Update deploy_pod_02.sh

--- lesson-02-openshift/deploy_pod_02.sh
+++ lesson-02-openshift/deploy_pod_02.sh
@@ -4,7 +4,7 @@
 # delete the pod if it exist
 oc delete pod testpod
 
-#export internal=$(oc registry info --internal=true)
+export internal=$(oc registry info --internal=true)
 
 #msg "--- Deploying image ibmpython to openshift internal registry at $internal/$PROJECT/ibmpython ---"
 
